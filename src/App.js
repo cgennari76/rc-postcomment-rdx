4021686a270fe209277584705b6ad71d2d742059
Chris <chrisgennari@gmail.com>
2022-09-30 15:49:31 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Navbar } from './app/Navbar';
+
+import { PostsList } from './features/posts/PostsList';
+import { AddPostForm } from './features/posts/AddPostForm';
+import { SinglePostPage } from './features/posts/SinglePostPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<><AddPostForm /><PostsList /></>}></Route>
+          <Route path="/posts/:postId" element={<SinglePostPage />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
